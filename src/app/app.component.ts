e486a572c848e88869a78a49429a90720d468f0f
Ladani Ankush <astarladani@gmail.com>
2022-02-24 09:32:51 +0530

    First-Day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { fn } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'First-Project';
+  // getName(name:any){
+  //   console.log('Function Called')
+  // }
+  getFullName(fname:any){
+    console.warn(fname);
+  }
 }
